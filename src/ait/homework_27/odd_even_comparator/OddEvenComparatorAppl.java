109a9f4bec85f1dc197252096ce28861ad4f9682
omordkovich <o.mordkovich@hotmail.com>
2024-08-14 22:10:58 +0200

    fixed homework 27

--- src/ait/homework_27/odd_even_comparator/OddEvenComparatorAppl.java
+++ src/ait/homework_27/odd_even_comparator/OddEvenComparatorAppl.java
@@ -6,33 +6,30 @@ import java.util.Comparator;
 public class OddEvenComparatorAppl {
     public static void main(String[] args) {
         Integer[] array = new Integer[10];
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * (100 - 1) + 1);
-        }
-
         Comparator<Integer> oddEvenComparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if ((o1 % 2 != 0 && o2 % 2 != 0)||(o1 % 2 == 0 && o2 % 2 == 0) ) {
-                    return o1.compareTo(o2);
-                }
-                if (o1 % 2 == 0) {
-                    return -1;
-                }
-                return 1;
+                return (o1 % 2 != 0) ? 1 : (o2 % 2 != 0) ? -1 : o1.compareTo(o2);
             }
         };
+        fillArray(array);
+        System.out.println("Source array:");
         printArray(array);
-        Arrays.sort(array,oddEvenComparator);
+        Arrays.sort(array, oddEvenComparator);
+        System.out.println("Sorted: first even (sorted) then odd (unsorted):");
         printArray(array);
 
+    }
+
+    private static void fillArray(Integer[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * (100 - 1) + 1);
+        }
     }
 
     private static void printArray(Integer[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
-
         }
         System.out.println();
     }
