ec6e45aa63f15493f76381c445761626319d8a7a
omordkovich <o.mordkovich@hotmail.com>
2024-09-16 22:25:07 +0200

    added tasks from consultation 16

--- src/ait/consultation_16/code/arrays_unique_elements/ArraysUniqueElements.java
+++ src/ait/consultation_16/code/arrays_unique_elements/ArraysUniqueElements.java
@@ -2,6 +2,7 @@ package ait.consultation_16.code.arrays_unique_elements;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -13,15 +14,22 @@ public class ArraysUniqueElements {
         //creating list of non-unique integers
         List<Integer> intArray = new ArrayList<>(Arrays.asList(1, 4, 5, 7, -5, 1, 2, 7, 1, 2, 23, 35, 0, 0));
         System.out.println(intArray.toString());
-        System.out.printf("number of unique elements is: %d",countUnique(intArray));
+        System.out.printf("number of unique elements is: %d", countUnique(intArray));
         System.out.println();
         //creating list of non-unique strings
-        List<String> strArray = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "one", "one", "three"));
+        List<String> strArray = new LinkedList<>(Arrays.asList("one", "two", "three", "four", "one", "one", "three"));
         System.out.println(strArray.toString());
-        System.out.printf("number of unique elements is: %d",countUnique(strArray));
+        System.out.printf("number of unique elements is: %d", countUnique(strArray));
+        System.out.println();
+        System.out.printf("number of unique elements in both lists is: %d", countMultiUnique(intArray, strArray));
     }
 
-    //method counts number of unique elements in Lists
+    //method counts number of unique elements in 2 lists
+    public static <I, S> int countMultiUnique(List<I> array1, List<S> array2) {
+        return countUnique(array1) + countUnique(array2);
+    }
+
+    //method counts number of unique elements in list
     public static <T> int countUnique(List<T> array) {
         int count = 0;  //setting count
         for (int i = 0; i < array.size(); i++) {
